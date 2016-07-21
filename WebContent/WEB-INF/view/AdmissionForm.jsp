df8e6086fcccb2ffd93ce01dae76e5c251f38581
thuya <thuyhatran@icloud.com>
2016-07-21 15:17:38 -0400

    Changed @RequestParam by @ModelAttribute

--- WebContent/WEB-INF/view/AdmissionForm.jsp
+++ WebContent/WEB-INF/view/AdmissionForm.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,11 +9,20 @@
 </head>
 <body>
 	
-	<form action="/SpringMVCStudentAdmission/submitAdmissionForm.html" method="post">
+	
+	<%-- <form  method="post"  action="/SpringMVCStudentAdmission/submitAdmissionForm.html">
 		Student's Name: <input type = "text" name="studentName" /> <br>
 		Student's Hobby: <input type = "text" name="studentHobby"/> <br>
 		<input type="submit" value="Submit" />
 	
-	</form>
+	</form> --%>
+	
+	 <form:form  method="post" modelAttribute="student" action="/SpringMVCStudentAdmission/submitAdmissionForm.html">
+		Student's Name: <form:input  path="name" /> <br>
+		Student's Hobby: <form:input path="hobby"/> <br>
+		<input type="submit" value="Submit" />
+	
+	</form:form> 
+	
 </body>
 </html>
--- src/com/mc/controller/StudentAdmissionController.java
+++ src/com/mc/controller/StudentAdmissionController.java
@@ -1,6 +1,7 @@
 package com.mc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,28 +13,41 @@ import com.mc.model.Student;
 public class StudentAdmissionController {
 	@RequestMapping(value="/admissionForm.html", method=RequestMethod.GET)
 	public ModelAndView getAdmissionForm(){
-		ModelAndView model = new ModelAndView("AdmissionForm");
+		ModelAndView model = new ModelAndView("AdmissionForm","student", new Student());
 		
 		return model;
 	}
 	
 	
+	
+//	@RequestMapping(value="/submitAdmissionForm.html", method=RequestMethod.POST)
+//	public ModelAndView submitAdmissionForm(@RequestParam("studentName") String student_name, @RequestParam("studentHobby") String student_hobby){
+//		// studentName, studentHobby are the same to the ones in AsmissionForm.jsp
+//		Student student1 = new Student();
+//		student1.setName(student_name);
+//		student1.setHobby(student_hobby);
+//		
+//		System.out.println("student 1:   " + student1);
+//		
+//		ModelAndView model = new ModelAndView("AdmissionSuccess");
+//		model.addObject("headerMessage","MT College");
+//		model.addObject("student",student1);
+//		
+//		return model;
+//		
+//	}
 	
 	@RequestMapping(value="/submitAdmissionForm.html", method=RequestMethod.POST)
-	public ModelAndView submitAdmissionForm(@RequestParam("studentName") String student_name, @RequestParam("studentHobby") String student_hobby){
-		// studentName, studentHobby are the same to the ones in AsmissionForm.jsp
-		Student student1 = new Student();
-		student1.setName(student_name);
-		student1.setHobby(student_hobby);
-		
-		System.out.println("student 1:   " + student1);
-		
-		ModelAndView model = new ModelAndView("AdmissionSuccess");
+	public ModelAndView submitAdmissionForm(@ModelAttribute("student") Student student1){
+		ModelAndView model = new ModelAndView("AdmissionSuccess");  //this is an .jsp file (see viewResolver)
 		model.addObject("headerMessage","MT College");
 		model.addObject("student",student1);
-		
 		return model;
 		
 	}
+	
+	
+	
+	
 	
 }
