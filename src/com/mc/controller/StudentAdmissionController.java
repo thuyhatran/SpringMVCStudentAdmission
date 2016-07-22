0cf06bc7ab5de2e151450ff6c8a2d1c17a030dc4
thuya <thuyhatran@icloud.com>
2016-07-22 12:18:44 -0400

    Don't need addObject(student) when using ModelAttribute

--- src/com/mc/controller/StudentAdmissionController.java
+++ src/com/mc/controller/StudentAdmissionController.java
@@ -1,9 +1,8 @@
 package com.mc.controller;
 
 import org.springframework.stereotype.Controller;
-
 import org.springframework.ui.Model;
-
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -47,11 +46,14 @@ public class StudentAdmissionController {
 	
 	@RequestMapping(value="/submitAdmissionForm.html", method=RequestMethod.POST)
 	public ModelAndView submitAdmissionForm(@ModelAttribute("student") Student student1){
+		
+		
 		ModelAndView model = new ModelAndView("AdmissionSuccess");  //this is an .jsp file (see viewResolver)
 		model.addObject("headerMessage","MT College");
-		model.addObject("student",student1);
+		//model.addObject("student",student1);
 		
 		return model;
+		
 	}
 	
 }
